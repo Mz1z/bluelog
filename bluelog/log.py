a4ae51ff179af80dea985dabc6e8ee2f54e90202
Mz1 <mzi_mzi@163.com>
2023-09-07 19:42:16 +0800

    update log.py

--- bluelog/log.py
+++ bluelog/log.py
@@ -11,9 +11,26 @@ basedir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 
 # 日志记录类
 class MzLog():
-	LOG_DB = os.path.join(basedir, 'log.db')
+	LOG_DB = os.path.join(basedir,'logs','log.db')
+
+	# 初始化数据库
+	@staticmethod
+	def init_db():
+		print("[init] log db path: " + MzLog.LOG_DB)
 	
 	# 记录访问
+	# 参数:
+	# 	文章id
+	# 	ip
+	# 	User-Agent
 	@staticmethod
-	def view_log():
-		pass
+	def view_log(**argv):
+		for arg in argv:  # debug
+			print(f'{arg} -> {argv[arg]}')
+
+
+
+# 测试
+if __name__ == '__main__':
+	MzLog.init_db()
+	MzLog.view_log(event="test")
